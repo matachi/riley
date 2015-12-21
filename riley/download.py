4003c0c04a26e0b315753b57ed746951ca8a2ee2
Daniel Jonsson <daniel@jonsson.xyz>
2015-12-22 00:25:32 +0100

    Add a download progress bar

--- riley/download.py
+++ riley/download.py
@@ -1,6 +1,8 @@
 import cgi
 import os
 from urllib.parse import urlparse
+
+from clint.textui.progress import Bar
 import requests
 
 
@@ -8,8 +10,11 @@ def download(url, to_dir):
     response = requests.get(url, stream=True)
     save_path = os.path.join(to_dir, get_file_name(response))
     with open(save_path, 'wb') as f:
-        for block in response.iter_content(1024):
-            f.write(block)
+        total_length = int(response.headers.get('content-length'))
+        with Bar(expected_size=total_length) as bar:
+            for i, block in enumerate(response.iter_content(1024), 1):
+                f.write(block)
+                bar.show(min(i * 1024, total_length))
 
 
 def get_file_name(response):
